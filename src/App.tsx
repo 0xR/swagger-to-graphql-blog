02379e10fcda0a37372e053ab7b6d5a1f72d16d7
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-09-18 17:54:46 +0200

    Add swagger to graphql to fetch pets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './App.css';
+import { createSchema } from './schema';
+import SchemaLink from 'apollo-link-schema';
+import { GraphQLSchema } from 'graphql';
+import gql from 'graphql-tag';
+import { useQuery, ApolloProvider } from '@apollo/react-hooks';
+
+function createApolloClient(schema: GraphQLSchema) {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new SchemaLink({
+      schema,
+    }),
+  });
+}
+
+type AppState =
+  | {
+      type: 'loading';
+    }
+  | {
+      type: 'error';
+      message: string;
+    }
+  | {
+      type: 'resolved';
+      client: ReturnType<typeof createApolloClient>;
+    };
+
+const PetsQuery = gql`
+  query {
+    findPetsByStatus(status: ["available", "pending"]) {
+      id
+      category {
+        name
+      }
+      status
+    }
+  }
+`;
+
+const Page = () => {
+  const { loading, error, data } = useQuery(PetsQuery, {});
+  return loading ? (
+    <>Loading pets...</>
+  ) : error ? (
+    <>Error loading pets {error}</>
+  ) : (
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  );
+};
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [state, setState] = useState<AppState>({
+    type: 'loading',
+  });
+
+  useEffect(() => {
+    async function fetchSchema() {
+      try {
+        const schema = await createSchema(
+          'https://petstore.swagger.io/v2/swagger.json',
+        );
+        setState({
+          type: 'resolved',
+          client: createApolloClient(schema),
+        });
+      } catch (e) {
+        setState({
+          type: 'error',
+          message: e.message,
+        });
+      }
+    }
+    fetchSchema();
+  }, []);
+
+  switch (state.type) {
+    case 'error':
+      return <>Error {state.message}</>;
+    case 'loading':
+      return <>Loading schema...</>;
+    case 'resolved':
+      return (
+        <ApolloProvider client={state.client}>
+          <Page />
+        </ApolloProvider>
+      );
+    default:
+      return (
+        <>Expected a different state than "{JSON.stringify(state, null, 2)}"</>
+      );
+  }
+};
 
 export default App;
