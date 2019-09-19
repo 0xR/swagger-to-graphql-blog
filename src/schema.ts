6ff68194bef276a4362c4fd7516839e5d9088420
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-09-19 17:36:07 +0200

    Simplify data fetching code

--- src/schema.ts
+++ src/schema.ts
@@ -3,43 +3,35 @@ import {
   CallBackendArguments,
 } from 'swagger-to-graphql';
 
+async function callBackend({
+  requestOptions: { method, body, baseUrl, path, query, headers },
+}: CallBackendArguments<{}>) {
+  const url = `${baseUrl}${path}?${new URLSearchParams(query as Record<
+    string,
+    string
+  >)}`;
+  const response = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  });
+
+  const text = await response.text();
+  if (200 <= response.status && response.status < 300) {
+    try {
+      return JSON.parse(text);
+    } catch (e) {
+      return text;
+    }
+  }
+  throw new Error(`Response: ${response.status} - ${text}`);
+}
+
 export const createSchema = (url: string) =>
   createSchemaFromSwagger({
     swaggerSchema: url,
-    async callBackend({
-      requestOptions: { method, body, baseUrl, path, query, headers, bodyType },
-    }: CallBackendArguments<{}>) {
-      const searchPath = query
-        ? `?${new URLSearchParams(query as Record<string, string>)}`
-        : '';
-      const url = `${baseUrl}${path}${searchPath}`;
-      const response = await fetch(url, {
-        method,
-        ...(body
-          ? {
-              ...(bodyType === 'json' && {
-                headers: {
-                  'Content-Type': 'application/json',
-                  ...headers,
-                },
-                body: JSON.stringify(body),
-              }),
-              ...(bodyType === 'formData' && {
-                headers,
-                body: new URLSearchParams(body),
-              }),
-            }
-          : { headers }),
-      });
-
-      const text = await response.text();
-      if (200 <= response.status && response.status < 300) {
-        try {
-          return JSON.parse(text);
-        } catch (e) {
-          return text;
-        }
-      }
-      throw new Error(`Response: ${response.status} - ${text}`);
-    },
+    callBackend,
   });
